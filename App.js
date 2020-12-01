e63fdf8fefb9d15fe11c10db9537258fdb55cfc0
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2020-12-01 17:19:36 +0700

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalHandler = (goalTitle) => {
+    if(goalTitle.length === 0) {
+      return;
+    }
+    setCourseGoals((currentGoals) => {
+      return [...currentGoals, { id: Math.random().toString(), value: goalTitle }];
+    });
+    setIsAddMode(false);
+  }
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => {
+        return goal.id !== goalId;
+      });
+    });
+  }
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title='Add New Goal' onPress={() => setIsAddMode(true)}></Button>
+      <GoalInput 
+        visible={isAddMode}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAdditionHandler}
+      />
+      <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals} 
+        renderItem={itemData => (
+        <GoalItem 
+          id={itemData.item.id}
+          title={itemData.item.value}
+          onDelete={removeGoalHandler}
+        />
+        )}
+      ></FlatList>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    padding: 50
+  }
 });
